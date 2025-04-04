34fc3cb63acf1fdefc9dbd245842f2b5448e7bf6
ianh-code <ian.n.hurd@gmail.com>
2025-04-04 11:58:26 -0500

    Completed lab1 assignment
    Completed lab1

---
+++ labs/lab1/lab1.py
@@ -0,0 +1,9 @@
+# A.I. Disclaimer: All work for this assignment was completed by myself and entirely without the
+# use of artificial intelligence tools such as ChatGPT, MS Copilot, other LLMs, etc.
+
+# -*- coding: utf-8 -*-
+import numpy as np
+a = 3
+b = 2
+c = np.multiply(a, b)
+print(c)
